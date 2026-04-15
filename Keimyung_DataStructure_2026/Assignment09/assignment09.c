174448ef2c645b1f3b94055422a0e42a80596fa5
Hyun-jun-Lee0811 <korong0811@naver.com>
2026-04-15 21:13:30 +0900

    Assignment 9 (2026-04-15)

---
+++ Keimyung_DataStructure_2026/Assignment09/assignment09.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include "arrayList.h"
+
+int main() {
+    arrayList* myArrayList = createArrayList(2);
+
+    insertArrayList(myArrayList, 0, 10);
+    insertArrayList(myArrayList, 1, 20);
+    printArrayList(myArrayList);
+    insertArrayList(myArrayList, 2, 30);
+    insertArrayList(myArrayList, 3, 40);
+    printArrayList(myArrayList);
+    insertArrayList(myArrayList, 4, 50);
+    printArrayList(myArrayList);
+    destroyArrayList(myArrayList);
+    return 0;
+}
